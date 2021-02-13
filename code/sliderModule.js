42ceddb8f681e9d3d76589562cba4d8cf91a05c4
h-yes-oo <hyesoo5115@naver.com>
2021-02-13 20:25:36 +0900

    modulization

---
+++ code/sliderModule.js
@@ -0,0 +1,8 @@
+var sliderBottom = document.getElementById('sliderBottom'),
+        sliderItemsBottom = document.getElementById('slidesBottom'),
+        prevBottom = document.getElementById('prevBottom'),
+        nextBottom = document.getElementById('nextBottom');
+    slide(sliderBottom, sliderItemsBottom, prevBottom, nextBottom);
+
+
+
